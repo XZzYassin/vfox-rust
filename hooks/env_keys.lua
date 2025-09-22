2de455b979c75a226db0da863129c51bcccc4ddf
Mumen Yassin <yassin.mumen@gmail.com>
2025-09-23 01:53:33 +0300

    Modernize environment variable hook for Rust

--- hooks/env_keys.lua
+++ hooks/env_keys.lua
@@ -4,30 +4,32 @@
 --- @param ctx table Context information
 --- @field ctx.path string SDK installation directory
 function PLUGIN:EnvKeys(ctx)
-    --- this variable is same as ctx.sdkInfo['plugin-name'].path
     local mainPath = ctx.path
-    local mainSdkInfo = ctx.main
-    local mpath = mainSdkInfo.path
-    local mversion = mainSdkInfo.version
-    local mname = mainSdkInfo.name
-    local sdkInfo = ctx.sdkInfo['sdk-name']
-    local path = sdkInfo.path
-    local version = sdkInfo.version
-    local name = sdkInfo.name
+
     return {
         {
-            key = "JAVA_HOME",
-            value = mainPath
+            key = 'PATH',
+            value = mainPath .. "/rustc/bin",
         },
         {
-            key = "PATH",
-            value = mainPath .. "/bin"
+            key = 'PATH',
+            value = mainPath .. "/cargo/bin",
         },
         {
-            key = "PATH",
-            value = mainPath .. "/bin2"
+            key = 'PATH',
+            value = mainPath .. "/clippy-preview/bin",
         },
-
+        {
+            key = 'PATH',
+            value = mainPath .. "/rust-analyzer-preview/bin",
+        },
+        {
+            key = 'PATH',
+            value = mainPath .. "/rustfmt-preview/bin",
+        },
+        {
+            key = 'CARGO_HOME',
+            value = mainPath .. '/cargo'
+        }
     }
-
 end
